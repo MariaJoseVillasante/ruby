176370b466f310bb4c6e894247855cf788b1171a
MariaJoseVillasante <pepavillasante@yahoo.com>
2022-07-18 10:47:22 -0400

    Agregando detalles a cuenta bancaria

--- introduccionObjetos/desafioGuiado/cuenta_bancaria.rb
+++ introduccionObjetos/desafioGuiado/cuenta_bancaria.rb
@@ -1,18 +1,18 @@
 class CuentaBancaria
     attr_accessor :nombre_de_usuario, :numero_de_cuenta, :vip
-    def initialize(nombre_de_usuario,numero_de_cuenta,vip=0)
+    def initialize(nombre_de_usuario,numero_de_cuenta,vip=0)#constructor
         @nombre_de_usuario=nombre_de_usuario
         @vip=vip
         begin 
             @numero_de_cuenta=numero_de_cuenta
-            raise ArgumentError 'El número de cuenta debe tener 8 digitos' if numero_de_cuenta.digits.count != 8
+            raise RangeError 'El número de cuenta debe tener 8 digitos' if numero_de_cuenta.digits.count != 8
         rescue
             @numero_de_cuenta='-----'
             puts 'Ingresa una cuenta de 8 digitos'
         end
     end
     def numero_de_cuenta
-        return "#{@vip}-#{@numero_de_cuenta}"
+        "#{@vip}-#{@numero_de_cuenta}" #return no es necesario
     end
 end 
 cuenta1 = CuentaBancaria.new('persona',123456789)
@@ -21,3 +21,4 @@ puts cuenta1.numero_de_cuenta
 cuenta2 = CuentaBancaria.new('persona2',12345678,1)
 puts cuenta2.nombre_de_usuario
 puts cuenta2.numero_de_cuenta
+puts cuenta3 = CuentaBancaria.new('persona3', 12345678).nombre_de_usuario
