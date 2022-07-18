5899709123672112cd263ff3646fc0b75bccf470
MariaJoseVillasante <pepavillasante@yahoo.com>
2022-07-18 11:41:21 -0400

    Agregando herencias

---
+++ oop/herencias/moto.rb
@@ -0,0 +1,5 @@
+class Moto < Vehiculo
+    def initialize
+        @ruedas=2
+    end
+end
---
+++ oop/herencias/vehiculo.rb
@@ -0,0 +1,14 @@
+class Vehiculo
+    attr_accessor :ruedas
+    def initialize
+        @ruedas = 4
+    end
+    def arrancar
+        puts 'rRRRRRrRRRRRrrR'
+    end
+    def detenerse
+        puts 'El motor se ha detenido...'
+    end
+ end
+
+ 
