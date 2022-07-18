4df7b07aca666ebb3efcc71565823fc827bbfe25
MariaJoseVillasante <pepavillasante@yahoo.com>
2022-07-18 14:50:00 -0400

    Terminando desafio evaluado

--- introduccionObjetos/desafio_evaluado/barajas.rb
+++ introduccionObjetos/desafio_evaluado/barajas.rb
@@ -9,7 +9,22 @@ class Baraja
                 @cartas.push(Carta.new(numero,letra).entrega)
             end
         end
-        return @cartas    
     end
 end
-print @cartas 
+# Al crear una baraja (constructor) se deben generar todas las combinaciones de
+# números y pinta y guardarse dentro del arreglo cartas.
+todas_las_opciones = Baraja.new
+#print todas_las_opciones.cartas
+# Crear el método barajar que debe desordenar el arreglo de cartas Tip: Utilizar
+# .shuffle.
+todas_las_opciones.cartas.shuffle
+# Crear el método sacar que permita retirar la primera carta de la baraja Tip: Utilizar
+# .pop. (1 Punto)
+print todas_las_opciones.cartas.shuffle.pop
+# Crear el método repartir mano que devolverá un arreglo con las primeras 5 cartas de
+# la baraja. (1 Punto)
+primeras = []
+5.times do |i|
+    primeras.push todas_las_opciones.cartas.shuffle.pop
+end
+print primeras
